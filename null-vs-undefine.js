a2e5fd962b08619a6f2949be0d10b691e1d799dc
shamimacse <shamima.cse01@gmail.com>
2021-02-03 23:14:40 +0600

    null vs undefine

---
+++ null-vs-undefine.js
@@ -0,0 +1,21 @@
+let student
+function add (num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+function add (num1, num2){
+// console.log(num1 + num2);
+console.log(num1 , num2);
+
+}
+// const result = add(14, 59);
+
+const student = {name: "shamima shema", phone: 1290};
+console.log(student.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let num =[2,6,7]
+console.log(num[10]);
