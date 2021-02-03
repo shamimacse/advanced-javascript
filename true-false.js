b06d09cee158eb68de041e04e66f1bf2da34b57a
shamimacse <shamima.cse01@gmail.com>
2021-02-03 20:08:51 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,57 @@
+// const name ="shamima"
+
+
+// if(name.length >0){
+//     console.log("condition is true " );
+// }
+
+// else{
+//     console.log("condition is false");
+// }
+
+// const name ="shamima";
+
+
+// if(name.length ){
+//     console.log("condition is true " );
+// }
+
+// else{
+//     console.log("condition is false");
+// }
+
+// const name ="shamima";
+
+
+// if(name){
+//     console.log("condition is true " );
+// }
+
+// else{
+//     console.log("condition is false");
+// }
+
+
+// const name =""
+// const name ="0";
+// let name;
+// let name=null;
+// let name = NaN;
+// let name=' ';
+// let name=' ';
+// let name=[];
+// let name=false;
+let name=20;
+
+if(name || name == 0){
+    console.log("condition is true " );
+}
+
+else{
+    console.log("condition is false");
+}
+
+
+
+
+
